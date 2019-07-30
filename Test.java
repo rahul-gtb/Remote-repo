1af7c475c5a629978c0ac63970553c3930ba0eb1
rahul-gtb <53329218+rahul-gtb@users.noreply.github.com>
2019-07-30 15:14:02 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,10 +8,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		
-		Calci obj=new Calci();
 		
-		System.out.println("adding numbers"+" "+obj.add(2,3));
-		System.out.println("dividing numbers"+" "+obj.divide(2,3));
 	
 
 	}
